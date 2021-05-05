e6f0aa14ff411b18ade021ccbbd32c422cdc0a9a
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-05 11:18:13 +0900

    FactoryBotの編集、テストコードの基盤作り

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    last_name             {'test'}
-    first_name            {'test'}
-    last_name_kana        {'test'}
-    first_name_kana       {'test'}
+    last_name             {Faker::Name.initials(number: 2)}
+    first_name            {Faker::Name.initials(number: 2)}
+    last_name_kana        {Faker::Name.initials(number: 2)}
+    first_name_kana       {Faker::Name.initials(number: 2)}
     birthday              {'2000/01/01'}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,15 +5,37 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it 'nicknameが空では登録できない' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    context '新規登録できるとき' do
+      it 'nicknameとemail、passwordとpassword_confirmation、last_nameとfirst_name、last_name_kanaとfirst_name_kana、birthdayが存在すれば登録できる' do
+      end
+      it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+      end
     end
-    it 'emailが空では登録できない' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Email can't be blank"
+    context '新規登録できないとき' do
+      it 'nicknameが空では登録できない' do
+      end
+      it 'emailが空では登録できない' do
+      end
+      it 'passwordが空では登録できない' do
+      end
+      it 'last_nameが空では登録できない' do
+      end
+      it 'first_nameが空では登録できない' do
+      end
+      it 'last_name_kanaが空では登録できない' do
+      end
+      it 'first_name_kanaが空では登録できない' do
+      end
+      it 'birthdayが空では登録できない' do
+      end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+      end
+      it '重複したemailが存在する場合登録できない' do
+      end
+      it 'emailに＠が存在しない場合登録できない' do
+      end
+      it 'passwordが5文字以下では登録できない' do
+      end
     end
   end
 end
