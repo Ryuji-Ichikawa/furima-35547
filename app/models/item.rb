643da499a285fbcb9e0696d3d07eb16389eac1c8
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-06 15:43:52 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+  
+  include ActiveHash::Associations
+  has_many :articles
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :price, format: { with: /\A[¥]?3[0-9]{2}|[¥]?[0-9]{7}+\z/}
+    validates :category_id
+    validates :status_id
+    validates :sipping_fee_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
+  
 end
