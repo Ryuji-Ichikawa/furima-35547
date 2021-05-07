b9de00e29dffa14ebf0d41df1f7b95fa8d31f655
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-07 12:45:12 +0900

    itemモデルの正規表現変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,10 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    validates :price, format: { with: /\A[¥]?3[0-9]{2}|[¥]?[0-9]{7}+\z/}
+    validates :price, format: { with: /\A3[0-9]{2}|[0-9]{7}+\z/}
     validates :category_id
     validates :status_id
-    validates :sipping_fee_id
+    validates :shipping_fee_id
     validates :prefecture_id
     validates :scheduled_delivery_id
   end
