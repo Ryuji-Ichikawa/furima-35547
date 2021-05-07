47ecd5f4052469ef96be809bb8402e31210d43bd
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-07 15:32:25 +0900

    itemテーブルのカラム名訂正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_034301) do
+ActiveRecord::Schema.define(version: 2021_05_07_055313) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2021_05_06_034301) do
     t.integer "price", null: false
     t.integer "category_id", null: false
     t.integer "status_id", null: false
-    t.integer "shippin_fee_id", null: false
+    t.integer "shipping_fee_id", null: false
     t.integer "prefecture_id", null: false
     t.integer "scheduled_delivery_id", null: false
     t.bigint "user_id", null: false
