d01f7183a703f412194a4acb6a9cab5e22771e74
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-07 12:41:46 +0900

    各プルダウンメニュー追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,10 @@
 class Article < ApplicationRecord
   
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :item
+  belongs_to :category
+  belongs_to :status
+  belongs_to :prefecture
+  belongs_to :shipping_fee
+  belongs_to :scheduled_delivery
+
 end
---
+++ app/models/category.rb
@@ -0,0 +1,16 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'ファッション' },
+    { id: 3, name: 'グルメ・飲料' },
+    { id: 4, name: '日用品・ヘルスケア' },
+    { id: 5, name: 'コスメ・ヘアケア' },
+    { id: 6, name: 'ベビー・キッズ' },
+    { id: 7, name: '家電' },
+    { id: 8, name: 'スポーツ・アウトドア' },
+    { id: 9, name: 'エンタメ・ホビー ' },
+    { id: 10, name: 'その他' }
+  ]
+  include ActiveHash::Associations
+  has_many :articles
+  end
