13dd585810918ade49de8e1358f4db63d96eb128
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-09 00:44:09 +0900

    テストの基盤作り
    itemモデルにバリデーションにimageを追加
    テストで使うimage画像の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :info
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid' }
