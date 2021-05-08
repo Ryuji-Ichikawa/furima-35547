cf249f74035583d1da867a9d6922bcdc5f7bfcf9
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-09 03:08:46 +0900

    販売手数料、利益計算の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,15 @@
 
-const priceInput = document.getElementById("item-price");
-console.log(priceInput);
-window.addEventListener('load', post );
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price")
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value
+    
+    const addTaxDom = document.getElementById("add-tax-price")
+    addTaxDom.innerHTML = Math.floor(inputValue / 10)
+
+    const profitDom = document.getElementById("profit")
+    const value_result = inputValue / 10
+    profitDom.innerHTML = Math.floor(inputValue - value_result)
+  })
+})
