d1745bd8af48c77d213417a72dbf0811014ac008
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-10 23:51:51 +0900

    コントローラーの作成

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,25 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @purchase = Purchase.create(purchase_params)
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def purchase_params
+    params.permit(:price).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name).merge(purchase_id: @purchase.id)
+  end
+end
