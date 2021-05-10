77c2c89ace824ff5259079a2d5c0d0f81dd507b3
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-10 18:29:17 +0900

    バリデーション処理

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,5 +2,13 @@ class PurchaseAddress
 
   include ActiveModel::Model
   attr_accessor :price, :user_id, :prefecture_id, :postal_code, :city, :addresse, :building
+  with_options presence: true do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                                      message: 'is invalid' }
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "is invalid"}
 
+  
 end
