d865a59ec5d18682cf276c9f7ed0c7275f0d1877
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-12 11:34:31 +0900

    コントローラーの変更
    newアクションの削除
    elseの際renderに変更
    before_actionの追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,8 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_item_id, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
-  end
-
-  def new
     @purchase_address = PurchaseAddress.new
   end
 
@@ -15,14 +12,17 @@ class PurchasesController < ApplicationController
       @purchase_address.save
       redirect_to root_path
     else
-      redirect_to item_purchases_path
+      render :index
     end
-  
   end
 
   private
   def purchase_params
     params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id).merge(user_id: current_user.id)
+  end
+
+  def set_item_id
+    @item = Item.find(params[:item_id])
   end
 
 end
