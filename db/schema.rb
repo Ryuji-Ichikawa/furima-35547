8586fc57345174a2eeeb39b0d7da3353d8ed4cc2
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-12 17:01:21 +0900

    マイグレーション、データベースの再作成

--- db/schema.rb
+++ db/schema.rb
@@ -34,30 +34,6 @@ ActiveRecord::Schema.define(version: 2021_05_12_063454) do
   end
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "item_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_addresses_on_item_id"
-    t.index ["user_id"], name: "index_addresses_on_user_id"
-  end
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "info", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "price", null: false
-    t.integer "category_id", null: false
-    t.integer "status_id", null: false
-    t.integer "shipping_fee_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "scheduled_delivery_id", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
     t.string "city", null: false
     t.string "address", null: false
@@ -67,7 +43,31 @@ ActiveRecord::Schema.define(version: 2021_05_12_063454) do
     t.bigint "purchase_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["purchase_id"], name: "index_purchases_on_purchase_id"
+    t.index ["purchase_id"], name: "index_addresses_on_purchase_id"
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "info", null: false
+    t.integer "price", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "shipping_fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "scheduled_delivery_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_purchases_on_item_id"
+    t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -89,7 +89,8 @@ ActiveRecord::Schema.define(version: 2021_05_12_063454) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "addresses", "items"
-  add_foreign_key "addresses", "users"
-  add_foreign_key "purchases", "purchases"
+  add_foreign_key "addresses", "purchases"
+  add_foreign_key "items", "users"
+  add_foreign_key "purchases", "items"
+  add_foreign_key "purchases", "users"
 end
